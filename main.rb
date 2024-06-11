e0ebab8b87b1da6d671f6b903969d9f127f8179e
vmg <musaravgd@gmail.com>
2024-06-11 18:40:48 +0200

    Created main file which serves as the entry point of the game. This is where the player choice which role to play.

--- main.rb
+++ main.rb
@@ -1,1 +1,17 @@
 # frozen_string_literal: true
+
+# Programme entry point
+
+require './mastermind'
+
+game = Mastermind.new
+
+puts "Do you want to play or should the computer ply instead? (Type 'play' or 'computer')"
+choice = gets.chomp.downcase
+if choice == 'play'
+  game.play
+elsif choice == 'computer'
+  game.computer_play
+else
+  puts 'Invalid choice'
+end
